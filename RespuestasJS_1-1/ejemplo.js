315228bcf3f9506739ab769ce224ee99f9bc763c
Maga <magalisantiago@hotmail.com>
2023-04-11 12:35:48 +0200

    first commit

---
+++ RespuestasJS_1-1/ejemplo.js
@@ -0,0 +1,53 @@
+//Ejercicios
+
+//1.
+let nombre = "Magali";
+let edad = 29;
+let esEstudiante = true;
+
+//2.
+let num1 = 2;
+let num2 = 18;
+
+console.log(`Suma de ${num1} y ${num2} = `, num1 + num2);
+console.log(`Resta de ${num1} y ${num2} =  `, num1 - num2);
+console.log(`Multiplicacion de ${num1} y ${num2} = `, num1*num2);
+console.log(`Division de ${num1} y ${num2} =  `, num1/num2);
+
+//3.
+let saludo = `Hola ${nombre}`;
+let saludo2 = "Hola " + nombre;
+console.log(saludo);
+console.log(saludo2);
+
+//4.
+
+let numeroComoString = "30";
+let numero = parseInt(numeroComoString);
+console.log(numero);
+console.log(numero*numero)
+
+//5.
+
+let valor1 = 45;
+let valor2 = 7;
+console.log("1 resultado = ", valor1==valor2);
+console.log("2 resultado = ", valor1!=valor2);
+console.log("3 resultado = ", valor1<valor2);
+console.log("4 resultado = ", valor1>valor2);
+console.log("5 resultado = ", valor1<=valor2);
+console.log("6 resultado = ", valor1>=valor2);
+
+//6.
+let condicion1 = true
+let condicion2 = false
+let condicion3 = true
+
+console.log(condicion1 && condicion2);
+console.log(condicion1 || condicion2);
+console.log(!condicion3);
+
+
+
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+
+//5.
+
+let valor1 = 45;
+let valor2 = 7;
+console.log("1 resultado = ", valor1==valor2);
+console.log("2 resultado = ", valor1!=valor2);
+console.log("3 resultado = ", valor1<valor2);
+console.log("4 resultado = ", valor1>valor2);
+console.log("5 resultado = ", valor1<=valor2);
+console.log("6 resultado = ", valor1>=valor2);
+
+//6.
+let condicion1 = valor1 && valor2;
+let condicion2 = valor1 || valor2;
+
+console.log("Resultado ",condicion1)
+console.log("Resultado ",condicion2)
