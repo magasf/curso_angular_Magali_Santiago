40eafb0585dfb267bfbf981e0df9d872ea4e48fe
Maga <magalisantiago@hotmail.com>
2023-04-13 11:25:33 +0200

    Agregando ejercicio anexo y avances en ejercicios de estructuras de datos

---
+++ RespuestasJs_1-4/EjercicioAnexo.js
@@ -0,0 +1,9 @@
+
+let preciosSinIVA = [4.9, 10.24, 40.99, 100.0];
+console.log(preciosSinIVA);
+
+//Obtener un array preciosConIVA (21%) utilizando la funcion map:
+
+let IVA = preciosSinIVA => preciosSinIVA * 1.21;
+let preciosConIVA = preciosSinIVA.map(IVA);
+console.log(preciosConIVA)
--- RespuestasJs_1-4/EstructurasDeDatos.js
+++ RespuestasJs_1-4/EstructurasDeDatos.js
@@ -11,7 +11,7 @@ let frutaAgregada1 = frutas.push("pomelo");
 console.log(frutas)
 let primeraFruta = frutas.shift();
 console.log(frutas);
-console.log(frutas.length)
+console.log(frutas.length);
 
 /**
  * 2. Iterar sobre un Array
@@ -21,4 +21,42 @@ console.log(frutas.length)
 
 let numeros = [1, 2, 3, 4, 5];
 
+
+  for (let i = 0; i < numeros.length; i++) {
+     const e = numeros[i];
+     console.log(e);
+  }
+ 
+for (let i = numeros.length-1; i > 0; i--) {
+   
+   console.log(numeros[i]);
+   
+}
+ for (let numero of numeros)
+  console.log(numero);
+
+/**
+   * 3. Uso de map, filter y reduce en Arrays
+      1. Crea un array llamado calificaciones que contenga calificaciones numéricas (por ejemplo, 85,
+      90, 78, 92, 88).
+      2. Utiliza el método map para aumentar cada calificación en 5 puntos.
+      3. Utiliza el método filter para quedarte solo con las calificaciones mayores o iguales a 90.
+      4. Utiliza el método reduce para calcular la calificación promedio
+*/
+
+let calificaciones = [85, 90, 78, 92, 88];
+console.log(calificaciones);
+
+let sumar5 = calificaciones => calificaciones + 5;
+console.log(calificaciones.map(sumar5));
+let calificacionesDefinitivas = calificaciones.map(sumar5);
+console.log(calificacionesDefinitivas);
+
+/**
+ * 4. Crear y manipular un Objeto
+      1. Crea un objeto llamado persona con las propiedades nombre, apellido, edad y ciudad.
+      2. Cambia el valor de la propiedad ciudad a una nueva ciudad.
+      3. Agrega una propiedad ocupacion al objeto.
+      4. Muestra en consola todas las propiedades y sus valores usando un bucle for…in.
+ */
 
