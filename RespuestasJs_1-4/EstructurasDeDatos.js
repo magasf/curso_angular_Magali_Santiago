314471afb80eeaadf082c2c9db485df1a15b3848
Maga <magalisantiago@hotmail.com>
2023-04-13 09:55:17 +0200

    Resolviendo el primer ejercicio de estructura de datos

---
+++ RespuestasJs_1-4/EstructurasDeDatos.js
@@ -0,0 +1,24 @@
+/**
+ * 1. Crear y manipular un Array
+    1. Crea un array llamado frutas que contenga tres frutas: "manzana", "plátano" y "naranja".
+    2. Agrega una fruta al final del array usando el método push.
+    3. Elimina la primera fruta del array usando el método shift.
+    4. Muestra en consola la longitud del array.
+ */
+
+let frutas = ["manzana", "platano", "naranja"];
+let frutaAgregada1 = frutas.push("pomelo");
+console.log(frutas)
+let primeraFruta = frutas.shift();
+console.log(frutas);
+console.log(frutas.length)
+
+/**
+ * 2. Iterar sobre un Array
+    1. Crea un array llamado numeros que contenga los números del 1 al 5.
+    2. Utiliza un bucle for para iterar sobre el array e imprimir cada número en consola.
+ */
+
+let numeros = [1, 2, 3, 4, 5];
+
+
