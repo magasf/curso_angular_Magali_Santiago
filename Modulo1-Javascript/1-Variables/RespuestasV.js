142f85cb56846bb4fdae5e4c190932de4623a75b
Maga <magalisantiago@hotmail.com>
2023-05-22 10:49:25 +0200

    Avances en el proyecto 13

--- Modulo1-Javascript/1-Variables/RespuestasV.js
+++ Modulo1-Javascript/1-Variables/RespuestasV.js
@@ -49,5 +49,3 @@ console.log(!condicion3);
 
 
 
-
-
--- Modulo3-Angular/6.AngularMaterial/proyecto13/src/app/app.module.ts
+++ Modulo3-Angular/6.AngularMaterial/proyecto13/src/app/app.module.ts
@@ -4,17 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserListComponent } from './user-list/user-list.component';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
 import { UserDirComponent } from './user-dir/user-dir.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { PeriodicElementsComponent } from './periodic-elements/periodic-elements.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
-    UserDirComponent
+    UserDirComponent,
+    UserDetailComponent,
+    PeriodicElementsComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
