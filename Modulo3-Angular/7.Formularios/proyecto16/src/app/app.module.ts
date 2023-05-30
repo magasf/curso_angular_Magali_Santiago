61c3d03def5f3786ccb9e1f6f07d7ddb35d94f8e
Maga <magalisantiago@hotmail.com>
2023-05-30 12:21:19 +0200

    Angular material, formularios password type dinamico

--- Modulo3-Angular/7.Formularios/proyecto16/src/app/app.module.ts
+++ Modulo3-Angular/7.Formularios/proyecto16/src/app/app.module.ts
@@ -10,6 +10,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +24,8 @@ import {MatIconModule} from '@angular/material/icon';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatCheckboxModule
 
   ],
   providers: [],
--- Modulo3-Angular/7.Formularios/proyecto16/src/app/user-form/user-form.component.ts
+++ Modulo3-Angular/7.Formularios/proyecto16/src/app/user-form/user-form.component.ts
@@ -7,12 +7,27 @@ import { FormControl,FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent {
+  hide = true;
   userForm = new FormGroup({
 
     fullName: new FormControl('',[Validators.required]),
     bio: new FormControl('', [Validators.required, Validators.maxLength(256)]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    isStudent: new FormControl(false)
+    isStudent: new FormControl(false),
+    acceptConditions: new FormControl(false, [Validators.requiredTrue]),
+    password: new FormControl('',[Validators.required, Validators.pattern('^[A-Za-z0-9]{8,20}$')])
+    //password con opcion de ver la contrasena
+    //varios campos en la misma linea
+    //formato nunmerico con prefijo
+    //datepicker
+    //stepper
+    //radio
+    //chips
+    //select
+    //autocomplete
+    //slide toggle
+    //slider
+
   });
 
   save(): void {
